5f219a46be298550363fbb26549a983e6dfe2cd0
fish895623 <dan990429@gmail.com>
2023-11-16 16:59:34 +0000

    fix: client variable & error log output

--- database/client.go
+++ database/client.go
@@ -1,28 +1,21 @@
 package database
 
 import (
-	"fmt"
 	"os"
+	"fmt"
 
 	"github.com/fish895623/bilf/types"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-const (
-	DB_USER     = "postgres"
-	DB_PASSWORD = "1234"
-	DB_NAME     = "hhhh"
-)
-
-var DBINFO = fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
 var DB *gorm.DB
 
 func init() {
 	var err error
 
 	if DB, err = gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{}); err != nil {
-		panic("Failed to connect to database")
+		fmt.Errorf("Failed to connect to database %w", err.Error())
 	}
 	DB.AutoMigrate(&types.Tag{}, &types.Daily{})
 }
