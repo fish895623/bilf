81fa1a97e0d61a9b3be1732daeb3afac96735a48
배성훈 <dan990429@gmail.com>
2023-11-28 17:51:24 +0900

    Default line feed is LF

--- route/root.go
+++ route/root.go
@@ -18,9 +18,14 @@ func (r CustomEngine) Routing() {
 		c.JSON(http.StatusOK, gin.H{"asdf": "asdf"})
 	})
 	g.GET("/ping", func(c *gin.Context) {
-		req, err := http.Get("https://finance.yahoo.com/quote/JEPI")
+		req, err := http.Get("https://finance.yahoo.com/quote/JEPII")
 		if err != nil {
 			log.Fatal(err.Error())
+			// TODO send error message when failed to connect
+			c.JSON(http.StatusOK, gin.H{
+				"status":       "failed",
+				"ErrorMessage": "Fail to get html", "log": err.Error(),
+			})
 		}
 		defer req.Body.Close()
 
@@ -29,6 +34,9 @@ func (r CustomEngine) Routing() {
 			log.Fatal(err.Error())
 		}
 
-		c.JSON(http.StatusOK, gin.H{"message": "pong", "DividendsPercentage": html.Find(`td[data-test="YTD_DTR-value"]`).Text()})
+		c.JSON(http.StatusOK, gin.H{
+			"status":              "ok",
+			"DividendsPercentage": html.Find(`td[data-test="YTD_DTR-value"]`).Text(),
+		})
 	})
 }
