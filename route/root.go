cfb8ae381f1a842ab0893caef268b5baf9262a3f
배성훈 <dan990429@gmail.com>
2024-02-28 10:24:39 +0900

    Refactor stock ping function for readability

--- route/root.go
+++ route/root.go
@@ -48,38 +48,6 @@ func CookieTool() gin.HandlerFunc {
 		c.Abort()
 	}
 }
-func ANYPingStocks(c *gin.Context) {
-	var req *http.Response
-	var html *goquery.Document
-
-	if c.Request.Method == "GET" {
-		req = RequestingQuote("JEPI")
-	} else if c.Request.Method == "POST" {
-		body := c.Request.Body
-		val, _ := io.ReadAll(body)
-		var data struct {
-			Name string `json:"name"`
-		}
-		json.Unmarshal([]byte(val), &data)
-
-		req = RequestingQuote(data.Name)
-	}
-	html, _ = goquery.NewDocumentFromReader(req.Body)
-	var data = html.Find(`td[data-test="YTD_DTR-value"]`).Text()
-	realData, _ := strconv.ParseFloat(strings.Replace(data, "%", "", -1), 64)
-
-	if data == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status": "error",
-			"error":  "invalid stock name",
-		})
-	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"status":              "ok",
-			"DividendsPercentage": realData,
-		})
-	}
-}
 
 type Price struct {
 	High float64 `json:"high"`
@@ -92,9 +60,4 @@ type ResponseData struct {
 	Week                Price   `json:"week"`
 	Year                Price   `json:"year"`
 	DividendsPercentage float64 `json:"DividendsPercentage"`
-}
-
-func RequestingQuote(name string) (res *http.Response) {
-	res, _ = http.Get(fmt.Sprintf("https://finance.yahoo.com/quote/%s", name))
-	return
 }
